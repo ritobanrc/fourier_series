a1f0588652e323688f7dfc6f3c622364c3de1d36
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-07-09 19:34:40 -0700

    using pycairo and gtk

--- main.py
+++ main.py
@@ -1,164 +1,129 @@
 import cmath
 import numpy as np
-
-from vispy import app, visuals
+import cairo
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib, Gdk
 from svgpathtools import svg2paths
 
+freqs =  list(range(-200, 200))
 shift_up = False
 
-def get_square_wave_coeff(n):
-    if n == 0: return 0
-    else:      return -2j/(n*cmath.pi)
+time = 0
+line = []
+coeffs = {}
+camera_scale = (0.5, 0.5)
 
-
-class Canvas(app.Canvas):
-    def __init__(self, svg, num_samples=10000, follow_path=True):
-        app.Canvas.__init__(self, keys='interactive', size=(800, 800), resizable=False)
-
-        self.follow_path = follow_path
-
-        self.time = 0
-        # dictionary from n to c_n
-        self.coeffs = {}
-        self.ellipses = {}
-        self.arrows = {}
-        self.ns = list(range(-40, 40))
-
-        paths, _, svg_attribs = svg2paths(svg, return_svg_attributes=True)
-        if 'viewBox' in svg_attribs:
-            *_, width, height = [int(x) for x in svg_attribs['viewBox'].split(' ')]
+def load_square_wave():
+    for n in freqs:
+        if n % 2 == 0:
+            coeffs[n] = 0
         else:
-            width = int(svg_attribs['width'])
-            height = int(svg_attribs['height'])
-        if len(paths) > 1:
-            print("Warning: SVG contains multiple paths, but only 1 is supported. Using only first path.")
-        path = paths[0]
-        assert path.isclosed()
-
-        dt = 1/num_samples
-
-        ts = np.linspace(0, 1, num_samples)
-        samples = np.empty([num_samples], dtype=complex)
-        for i, t in enumerate(ts):
-            p = path.point(t)
-            samples[i] = p.real/width + -1j * p.imag / height
-
-        self.camera = visuals.transforms.STTransform([0.75, 0.75])
-
-        for n in self.ns:
-            # c_n = np.array([cmath.exp(-n*cmath.tau*1j*t)*cs
-            # for t, cs, in zip(ts, samples)
-            # ]).sum() * dt
-            c_n = np.sum(np.exp(-n*cmath.tau*1j*ts)*samples) * dt
-            (r, phi) = cmath.polar(c_n)
-            self.ellipses[n] = visuals.EllipseVisual(
-                center=(0, 0),
-                color=(0, 0, 0, 0),
-                border_color=(0.5, 0.5, 0.5, 1.0),
-                radius=(r, r),
-                # antialias=True
-            )
-            self.ellipses[n].transform = self.camera
-            self.arrows[n] = visuals.ArrowVisual(pos=np.zeros((2, 2)),
-                                                 color=(1.0, 1.0, 1.0, 1.0),
-                                                 method='gl',
-                                                 antialias=True)
-            self.arrows[n].transform = self.camera
-
-            self.coeffs[n] = c_n
-
-        self.ns.sort(key=lambda n: cmath.polar(self.coeffs[n])[0], reverse=True)
-
-        self.line = visuals.LineVisual(pos=np.zeros((1, 2)),
-                                       color=(1.0, 1.0, 0.0, 1.0),
-                                       width=2,
-                                       method='gl',
-                                       antialias=True
-                                       )
-        self.line.transform = self.camera
-
-        self._timer = app.Timer('auto', connect=self.on_timer, start=True)
-        self.context.set_clear_color((0.1, 0.1, 0.1, 1.0))
-        self.show()
-
-    def on_draw(self, ev):
-        # self.context.set_viewport(0, 0, *self.physical_size)
-        self.context.clear()
-        for _, ellipse in self.ellipses.items():
-            ellipse.draw()
-        for _, arrow in self.arrows.items():
-            arrow.draw()
-        self.line.draw()
-
-    def on_resize(self, event):
-        vp = (0, 0, *self.physical_size)
-        self.context.set_viewport(*vp)
-
-    def on_timer(self, event):
-        self.time += 0.001
-        end_point = 0 + 0j
-        for n in self.ns:
-            self.ellipses[n].center = (end_point.real, end_point.imag)
-            temp = end_point
-            end_point += self.coeffs[n] * cmath.exp(n*cmath.tau*1j*self.time)
-            self.arrows[n].set_data(np.array([[temp.real, temp.imag],
-                                              [end_point.real, end_point.imag]]))
+            coeffs[n] = -2j/(n*cmath.pi)
 
 
-        if shift_up:
-            new_line = self.line.pos + np.array([0, 0.01])
-        else:
-            new_line = self.line.pos
+def load_svg_coeffs(filename, num_samples=10000):
+    paths, _, svg_attribs = svg2paths(filename, return_svg_attributes=True)
+    if 'viewBox' in svg_attribs:
+        *_, width, height = [int(float(x)) for x in svg_attribs['viewBox'].split(' ')]
+    else:
+        width = int(float(svg_attribs['width']))
+        height = int(float(svg_attribs['height']))
+    if len(paths) > 1:
+        print("Warning: SVG contains multiple paths, but only 1 is supported. Using only first path.")
+    path = paths[0]
 
-        delta = new_line[-1] - np.array([end_point.real, end_point.imag]) 
-        if new_line.shape[0] == 1:
-            new_line[0] = [end_point.real, end_point.imag]
-        new_line = np.vstack((new_line, [end_point.real, end_point.imag]))
-        self.line.set_data(new_line)
+    dt = 1/num_samples
 
-        if self.follow_path:
-            self.camera.move(self.camera.scale[0:1]*delta)
+    ts = np.linspace(0, 1, num_samples)
+    samples = np.empty([num_samples], dtype=complex)
+    for i, t in enumerate(ts):
+        p = path.point(t)
+        samples[i] = p.real/width + 1j * p.imag / height
 
-
-        self.update()
-
-    @property
-    def canvas_tr(self):
-        return visuals.transforms.STTransform.from_mapping(
-            [(0, 0), self.size],
-            [(-1, 1), (1, -1)])
-
-    def on_mouse_move(self, event):
-        """Mouse move handler
-        Parameters
-        ----------
-        event : instance of Event
-            The event.
-        """
-        if event.is_dragging:
-            dxy = event.pos - event.last_event.pos
-            button = event.press_event.button
-
-            if button == 1:
-                dxy = self.canvas_tr.map(dxy)
-                o = self.canvas_tr.map([0, 0])
-                t = dxy - o
-                self.camera.move(t)
-            elif button == 2:
-                center = self.canvas_tr.map(event.press_event.pos)
-                if self.camera._aspect is None:
-                    self.camera.zoom(np.exp(dxy * (0.01, -0.01)), center)
-                else:
-                    s = dxy[1] * -0.01
-                    self.camera.zoom(np.exp(np.array([s, s])), center)
+    for n in freqs:
+        coeffs[n] = np.sum(np.exp(-n*cmath.tau*1j*ts)*samples) * dt
 
 
-    def on_mouse_wheel(self, event):
-        zoom = np.exp(-event.delta[1] * -0.1)
-        self.camera.zoom([zoom, zoom], self.canvas_tr.map(event.pos))
+def draw(da, ctx):
+    """
+    The draw loop. Called once per frame and draws stuff
+    """
+    global time
+    alloc = da.get_allocation()
+    width = alloc.width
+    height = alloc.height
+
+    ctx.scale(camera_scale[0]*width/2, camera_scale[1]*height/2)
+    ctx.translate(2, 2)
+    ctx.set_line_width(0.003)
+
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.paint()
+
+    end_point = 0 + 0j
+    for n in freqs:
+        # self.ellipses[n].center = (end_point.real, end_point.imag)
+        ctx.set_source_rgb(0.4, 0.4, 0.5)
+        radius, _ = cmath.polar(coeffs[n])
+        ctx.arc(end_point.real, end_point.imag, radius, 0, cmath.tau)
+        ctx.stroke()
+        ctx.move_to(end_point.real, end_point.imag)
+
+        end_point += coeffs[n] * cmath.exp(n*cmath.tau*1j*time)
+
+        ctx.set_source_rgb(0.8, 0.8, 0.8)
+        ctx.line_to(end_point.real, end_point.imag)
+        ctx.stroke()
+
+    if shift_up:
+        for idx, _ in enumerate(line):
+            line[idx][1] += 0.005
+
+    line.append([end_point.real, end_point.imag])
+
+    ctx.set_source_rgb(1.0, 1.0, 0)
+    for point in line:
+        ctx.line_to(*point)
+    ctx.stroke()
+
+    time += 0.002
+
+
+def zoom(da, event):
+    global camera_scale
+    # print(event.x, event.y, event.direction)
+    if event.direction == Gdk.ScrollDirection.UP:
+        camera_scale = (camera_scale[0]*1.25, camera_scale[1]*1.25)
+    elif event.direction == Gdk.ScrollDirection.DOWN:
+        camera_scale = (camera_scale[0]*0.8, camera_scale[1]*.8)
+
+def main():
+    """
+    The main function
+    """
+    win = Gtk.Window()
+    win.connect('destroy', Gtk.main_quit)
+    win.set_default_size(800, 800)
+
+    drawingarea = Gtk.DrawingArea()
+    win.add(drawingarea)
+    drawingarea.connect('draw', draw)
+    drawingarea.add_events(Gdk.EventMask.SCROLL_MASK)
+    drawingarea.connect('scroll-event', zoom)
+
+    def tick():
+        drawingarea.queue_draw()
+        return True
+    GLib.timeout_add(20, tick)
+
+    load_svg_coeffs('treble.svg')
+
+    freqs.sort(key=lambda n: cmath.polar(coeffs[n])[0], reverse=True)
+
+    win.show_all()
+    Gtk.main()
+
 
 if __name__ == '__main__':
-    canvas = Canvas('treble.svg')
-    canvas.measure_fps()
-    canvas.app.run()
-
+    main()
